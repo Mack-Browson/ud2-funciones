69dd0ebde3740421fbbc4df3637aaf29fdd3572a
Unknown <obybrown65@gmail.com>
2017-11-13 19:15:46 +0100

    factorial

---
+++ 08_factorial/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+        public static long calcularFactorial(long n){
+
+            long factorial = 1;
+            if (n >= 0){
+
+                for (int i = 1; i<=n; i++){
+                    factorial *=i;
+                }
+
+            }
+
+            return factorial;
+        }
+    public static void main(String[] args) throws IOException {
+	// recibir datos
+
+        BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+
+        System.out.println (" dame un valor a calcular su factorial :");
+        int n = Integer.parseInt (br.readLine ());
+        do {
+            System.out.println (" El factorial es : "+calcularFactorial (n));
+        }while (!(n>=0));
+
+    }
+}
